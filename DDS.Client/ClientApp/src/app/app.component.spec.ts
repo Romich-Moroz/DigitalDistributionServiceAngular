76cc21eeed667793ad9b5bc1e30e924302ae805a
Roman <romich651@gmail.com>
2021-05-03 23:02:35 +0300

    fix client name

--- DDS.Client/ClientApp/src/app/app.component.spec.ts
+++ DDS.Client/ClientApp/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ClientApp'`, () => {
+  it(`should have as title 'Digital Distribution Service'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ClientApp');
+    expect(app.title).toEqual('Digital Distribution Service');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ClientApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Digital Distribution Service is running!');
   });
 });
